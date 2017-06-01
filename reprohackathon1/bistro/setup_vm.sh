5668bdf35e2bd4022e0824323fe9b9d1c6ee3e62
Philippe Veber <philippe.veber@gmail.com>
2017-06-01 14:43:49 +0200

    improve install script

--- reprohackathon1/bistro/setup_vm.sh
+++ reprohackathon1/bistro/setup_vm.sh
@@ -1,4 +1,11 @@
 apt update
-apt install -y opam emacs
-opam switch 4.04.0
+apt install -y emacs git aspcud utop
+
+wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin
+/usr/local/bin/opam init --comp 4.04.0
+
+eval `opam config env`
+
 opam pin add bistro --dev-repo
+
+#opam install tuareg merlin ocp-indent
