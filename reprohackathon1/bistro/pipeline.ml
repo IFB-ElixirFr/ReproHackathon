1466c59e5e1c5e7de701e2f890ffd48a02bf5e95
Philippe Veber <philippe.veber@gmail.com>
2017-06-01 15:51:56 +0200

    fix star index wrapper

--- reprohackathon1/bistro/pipeline.ml
+++ reprohackathon1/bistro/pipeline.ml
@@ -19,6 +19,7 @@ module Star = struct
 
   let index (fa : fasta workflow) =
     workflow ~descr:"star.index" ~np:8 ~mem:(10 * 1024) [
+      mkdir_p dest ;
       cmd "STAR" ~env [
         opt "--runThreadN" ident np ;
         opt "--runMode" string "genomeGenerate" ;
@@ -97,6 +98,33 @@ let opt_mapped_reads idx (sra : sra workflow) =
       opt "--limitBAMsortRAM" ident mem ;
     ]
   ]
+
+(* let sra_head_to_fastq sra = *)
+(*   workflow ~descr:"sra_head_to_fastq" [ *)
+(*     mkdir_p tmp ; *)
+(*     cmd ~env:sratoolkit_env "fastq-dump" [ *)
+(*       opt "-O" ident tmp ; *)
+(*       string "--split-files" ; *)
+(*       dep sra *)
+(*     ] ; *)
+(*     mv (tmp // "*_1.fastq.gz") (fqgz 1) ; *)
+(*     mv (tmp // "*_2.fastq.gz") (fqgz 2) ; *)
+(*     cmd "STAR" ~stdout:dest ~env:Star.env [ *)
+(*       opt "--runThreadN" ident np ; *)
+(*       opt "--outSAMstrandField" string "intronMotif" ; *)
+(*       opt "--outFilterMismatchNmax" int 4 ; *)
+(*       opt "--outFilterMultimapNmax" int 10 ; *)
+(*       opt "--genomeDir" dep idx ; *)
+(*       opt "--readFilesIn" ident (seq ~sep:" " [ gunzip (fqgz 1) ; *)
+(*                                                 gunzip (fqgz 2) ]) ; *)
+(*       opt "--outSAMunmapped" string "None" ; *)
+(*       opt "--outSAMtype" string "BAM SortedByCoordinate" ; *)
+(*       opt "--outStd" string "BAM_SortedByCoordinate" ; *)
+(*       opt "--genomeLoad" string "NoSharedMemory" ; *)
+(*       opt "--limitBAMsortRAM" ident mem ; *)
+(*     ] *)
+(*   ] *)
+
 
 let dexseq_script = {rscript|
 library(DEXSeq)
