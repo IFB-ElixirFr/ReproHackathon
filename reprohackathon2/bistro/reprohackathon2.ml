24e70829554e54751fd98ca7f991fa3aa7e8538a
Philippe Veber <philippe.veber@gmail.com>
2018-07-09 17:51:31 +0200

    4 methods work, and tree compare

--- reprohackathon2/bistro/reprohackathon2.ml
+++ reprohackathon2/bistro/reprohackathon2.ml
@@ -61,7 +61,7 @@ module IQTree = struct
             string "-seed 1" ;
             opt "-nt" (fun x -> x) np ;
           ] ;
-          mv (tmp // (tmp_ali_fn ^ ".iqtree")) dest ;
+          mv (tmp // (tmp_ali_fn ^ ".treefile")) dest ;
         ]
       )
     ]
@@ -71,32 +71,63 @@ module PhyML = struct
   let env = docker_image ~account:"pveber" ~name:"phyml" ~tag:"3.3.20180129" ()
 
   let phyml alignment =
+    let tmp_ali_fn = "alignment" in
+    let tmp_ali = tmp // tmp_ali_fn in
     shell ~descr:"phyml" [
       docker env (
         and_list [
           cd tmp ;
+          cmd "ln" [ string "-s" ; dep alignment ; tmp_ali ] ;
           cmd "/usr/local/bin/phyml" [
-            opt "-i" dep alignment ;
+            opt "-i" (fun x -> x) tmp_ali ;
             string "--r_seed 1 -d nt -b 0 -m GTR -f e -c 4 -a e -s SPR --n_rand_starts 1 -o tlr -p --run_id ID" ;
           ] ;
-          mv (tmp // "*_phyml_tree_ID.txt") dest ;
+          mv (tmp // (tmp_ali_fn ^ "*_phyml_tree_ID.txt")) dest ;
         ]
       )
     ]
 end
 
+module Goalign = struct
+  let env = docker_image ~account:"pveber" ~name:"goalign" ~tag:"0.2.9" ()
+
+  let phylip_of_fasta fa =
+    shell ~descr:"goalign.reformat" [
+      cmd "goalign" ~env [
+        string "reformat phylip" ;
+        opt "-i" dep fa ;
+        opt "-o" (fun x -> x) dest ;
+      ]
+    ]
+end
+
+module Gotree = struct
+  let env = docker_image ~account:"pveber" ~name:"gotree" ~tag:"0.2.10" ()
+
+  let compare_trees ~input ~compare =
+    shell ~descr:"gotree.compare" [
+      cmd "/usr/local/bin/gotree" ~stdout:dest ~env [
+        string "compare trees --binary" ;
+        opt "-i" dep input ;
+        opt "-c" dep compare ;
+      ]
+    ]
+end
+
+
 let tree_inference fa = function
   | `Fasttree -> Fasttree.fasttree fa
   | `RAXML -> Raxml.hpc fa
   | `IQTree -> IQTree.iqtree fa
-  | `PhyML -> PhyML.phyml fa
+  | `PhyML -> PhyML.phyml (Goalign.phylip_of_fasta fa)
 
 (* let gene_alignments = glob dataset  *)
 
 let random_alignment = input "single-gene_alignments/SongD1/gene100.aln"
 
 let repo = Repo.[
-    item ["random_tree.nhx"] (tree_inference random_alignment `IQTree) ;
+    item ["random_tree.nhx"] (tree_inference random_alignment `PhyML) ;
+    item ["random_comparison"] (Gotree.compare_trees ~input:(tree_inference random_alignment `IQTree) ~compare:(tree_inference random_alignment `RAXML))
   ]
 
 let () = Repo.build ~loggers:[console_logger ()] ~np:4 ~mem:(`GB 4) ~outdir:"res" repo
