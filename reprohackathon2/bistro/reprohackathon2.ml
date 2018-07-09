872629067f17b9d7edda0034cfdfef923c2f53aa
Philippe Veber <philippe.veber@gmail.com>
2018-07-09 16:36:29 +0200

    bistro: raxml works

--- reprohackathon2/bistro/reprohackathon2.ml
+++ reprohackathon2/bistro/reprohackathon2.ml
@@ -14,7 +14,23 @@ let dataset =
   |> Bistro_unix.bunzip2
 
 module Raxml = struct
-  
+  let env = docker_image ~account:"pveber" ~name:"raxml" ~tag:"8.2.9" ()
+
+  let hpc alignment =
+    shell ~descr:"raxmlhpc" ~np:4 [
+      docker env (
+        and_list [
+          cd tmp ;
+          cmd "raxmlHPC" [
+            opt "-T" (fun x -> x) np ;
+            string "-p 1 -m GTRGAMMA --no-bfgs" ;
+            opt "-s" dep alignment ;
+            string "-n NAME" ;
+          ] ;
+        ]
+      ) ;
+      mv (tmp // "RAxML_bestTree.NAME") dest ;
+    ]
 end
 
 module Fasttree = struct
@@ -29,17 +45,31 @@ module Fasttree = struct
     ]
 end
 
+module IQTree = struct
+  let env = docker_image ~account:"pveber" ~name:"iqtree" ~tag:"1.4.2" ()
+
+  let iqtree fa =
+    shell ~descr:"iqtree" [
+      cmd ~env "/usr/local/bin/iqtree" [
+        string "-m GTR+64" ;
+        opt "-s" dep fa ;
+        string "-seed 1" ;
+        opt "-nt" (fun x -> x) np ;
+      ]
+    ]
+end
 
 let tree_inference fa = function
   | `Fasttree -> Fasttree.fasttree fa
+  | `RAXML -> Raxml.hpc fa
+  | `IQTree -> IQTree.iqtree fa
 
 (* let gene_alignments = glob dataset  *)
 
 let random_alignment = input "single-gene_alignments/SongD1/gene100.aln"
 
 let repo = Repo.[
-    item ["random_tree.nhx"] (tree_inference random_alignment `Fasttree) ;
-    item ["random_ali.nhx"] random_alignment ;
+    item ["random_tree.nhx"] (tree_inference random_alignment `RAXML) ;
   ]
 
 let () = Repo.build ~loggers:[console_logger ()] ~np:4 ~mem:(`GB 4) ~outdir:"res" repo
