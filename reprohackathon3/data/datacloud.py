5175949d8d31da99aadefd400cf45b335bc75eeb
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2019-11-26 16:09:56 +0100

    debug reading of small index csv file

--- reprohackathon3/data/datacloud.py
+++ reprohackathon3/data/datacloud.py
@@ -10,8 +10,8 @@ PREFIX='/ifb/data/public/teachdata/reprohack3/ARCH2016-04-15/binaries'
 ## read index
 def read_index(prefix=os.path.join(PREFIX,'..')):
 	df = pandas.read_csv(os.path.join(prefix, 'ZA16_organized_database.csv'),sep=';')
-	for col in ('path_http','camera_angle', 'dates', 'view_type'):
-		df.loc[:,col] = map(eval, df.loc[:,col])
+#	for col in ('path_http','camera_angle', 'dates', 'view_type'):
+#		df.loc[:,col] = map(eval, df.loc[:,col])
 	return df
 
 ## binary path
@@ -22,13 +22,13 @@ def get_remote_path_bin(row, prefix=PREFIX, delta=0):
     date = date + datetime.timedelta(seconds=delta)
     str_date = date.strftime('%Y-%m-%d_%H-%M-%S')
 
-    str_views = ['sv' if v == 'side' else 'tv' for v in row["view_type"]]
+    str_views = ['sv' if v == 'side' else 'tv' for v in eval(row["view_type"])]
 
     filenames = list()
-    for filename, view, angle, date in zip(row["path_http"],
+    for filename, view, angle, date in zip(eval(row["path_http"]),
                                            str_views,
-                                           row["camera_angle"],
-                                           row["dates"]):
+                                           eval(row["camera_angle"]),
+                                           eval(row["dates"])):
 
         r = filename.split('/')
 
@@ -57,15 +57,15 @@ def get_bin_images(row, prefix=PREFIX):
 			break
 	if delta == 4:
 		raise ValueError('Can not find binaries')
-	imdict={t:{} for t in set(row.view_type)}
-	for view, angle, path in zip(row.view_type,row.camera_angle,filenames):
+	imdict={t:{} for t in set(eval(row.view_type))}
+	for view, angle, path in zip(eval(row.view_type),eval(row.camera_angle),filenames):
 		imdict[view][angle]=cv2.imread(path, cv2.IMREAD_UNCHANGED)
 	return imdict
 
 
 def get_calibrations(row, prefix=os.path.join(PREFIX,'..','calibration')):
 	calib = {}
-	for view in set(row.view_type):
+	for view in set(eval(row.view_type)):
 		path = os.path.join(prefix, (row.cabin + '_' + view + '_1_1_wide.json'))
 		calib[view] = CalibrationCamera.load(path)
 	return calib
