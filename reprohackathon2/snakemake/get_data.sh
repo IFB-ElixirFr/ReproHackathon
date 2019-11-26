22ad3ef4b224beeed872f90de2c479ac185363df
jolorenzo <jonathan.lorenzo@france-bioinformatique.fr>
2019-11-26 10:33:04 +0100

    Update repo

--- reprohackathon2/snakemake/get_data.sh
+++ reprohackathon2/snakemake/get_data.sh
@@ -13,7 +13,7 @@ if [ ! -d /data/single-gene_alignments ]; then
     cd /data
     tar xjf single-gene_alignments.tar.bz2
 fi
-if [ ! -d /data/single-gene_alignments ]; then
+if [ ! -d /data/single-gene_tree ]; then
     wget -O /data/single-gene_tree.tar.bz2 https://ndownloader.figshare.com/files/9473953
     cd /data
     tar xjf single-gene_tree.tar.bz2
