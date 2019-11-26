b52933b8cbe45ae9b1adf00e55df1f0aa99f398c
unknown <pierre-edouard.guerin@laposte.net>
2019-11-26 16:57:44 +0100

    snakemake

--- reprohackathon3/guerin_workflow/reconstruct.py
+++ reprohackathon3/guerin_workflow/reconstruct.py
@@ -1,13 +1,17 @@
-db = dc.read_index()
-row_number = 2000# Available : 0, 1, 2, 3, 4 or 5, or  ... 57436
-row = db.loc[row_number]
-bin_images = dc.get_bin_images(row)
-calibrations = dc.get_calibrations(row)
 #==============================================================================
 #MODULES
 #==============================================================================
-import datacloud as dc
+%matplotlib notebook
 
+import cv2
+
+import openalea.phenomenal.data as phm_data 
+import openalea.phenomenal.display as phm_display
+import openalea.phenomenal.object as phm_obj
+import openalea.phenomenal.multi_view_reconstruction as phm_mvr
+import openalea.phenomenal.mesh as phm_mesh
+import openalea.phenomenal.display.notebook as phm_display_notebook
+import datacloud as dc
 import cv2
 import os
 import datetime
@@ -18,34 +22,92 @@ from openalea.phenomenal.calibration import CalibrationCamera
 #FUNCTIONS
 #==============================================================================
 
-## read index
-def read_index_chunk(index_file):
-    df = pandas.read_csv(index_file, sep=';')
-    for col in ('path_http','camera_angle', 'dates', 'view_type'):
-        exec("templist="+df.loc[0][col])
-        df.loc[0][col] = templist
-	return df
-
 
 #==============================================================================
 #ARGUMENTS
 #==============================================================================
 parser = argparse.ArgumentParser(description='DEMORT - DEmultiplexing MOnitoring Report Tool')
-parser.add_argument("-i","--index_file", type=str)
-parser.add_argument("-o","--output_print", type=str)
-
+parser.add_argument("-r","--index_row", type=str)
 
 args = parser.parse_args()
-index_file = args.index_file
-output_print = args.output_print
+index_row = args.index_row
 
-index_file="./indexes/ligne_2000.csv"
-
-selection_plants= [2000, 1, 2, 3]
+db = dc.read_index()
 
 
-dbc=read_index_chunk(index_file)
-
-row = dbc.loc[0]
+row = db.loc[index_row]
 bin_images = dc.get_bin_images(row)
 calibrations = dc.get_calibrations(row)
+
+#==============================================================================
+# MULTIVIEW
+#==============================================================================
+
+
+## Prerequisites
+###############################################################################
+## LOAD DATA
+plant_number = 2# Available : 1, 2, 3, 4 or 5
+bin_images = phm_data.bin_images(plant_number=plant_number)
+calibrations = phm_data.calibrations(plant_number=plant_number)
+
+phm_display.show_images(bin_images['side'].values() + bin_images['top'].values())
+
+## Multi-view reconstruction
+###############################################################################
+### Associate images and projection function
+def routine_select_ref_angle(bin_side_images):
+    max_len = 0
+    max_angle = None    
+    for angle in bin_side_images:    
+        x_pos, y_pos, x_len, y_len = cv2.boundingRect(cv2.findNonZero(bin_side_images[angle]))
+        if x_len > max_len:
+            max_len = x_len
+            max_angle = angle
+    return max_angle
+
+refs_angle_list = [routine_select_ref_angle(bin_images["side"])]
+
+image_views = list()
+for id_camera in bin_images:
+    for angle in bin_images[id_camera]:
+        projection = calibrations[id_camera].get_projection(angle)
+    
+        image_ref = None
+        if id_camera == "side" and angle in refs_angle_list:
+            image_ref = bin_images[id_camera][angle]
+        
+        inclusive = False
+        if id_camera == "top":
+            inclusive = True
+            
+        image_views.append(phm_obj.ImageView(
+            bin_images[id_camera][angle], 
+            projection, 
+            inclusive=inclusive, 
+            image_ref=image_ref))
+
+##### Do multi-view reconstruction
+voxels_size = 16 # mm
+error_tolerance = 0
+voxel_grid = phm_mvr.reconstruction_3d(image_views, 
+                                       voxels_size=voxels_size,
+                                       error_tolerance=error_tolerance)
+
+
+
+###### Save / Load voxel grid
+voxel_grid.write("voxel_grids/plant_{}_size_{}.npz".format(plant_number, voxels_size))
+voxel_grid = phm_obj.VoxelGrid.read("plant_{}_size_{}.npz".format(plant_number, voxels_size))
+
+##### Viewing
+#phm_display_notebook.show_voxel_grid(voxel_grid, size=1)
+
+## meshing
+vertices, faces = phm_mesh.meshing(voxel_grid.to_image_3d(),
+                                   reduction=0.90,
+                                   smoothing_iteration=5,
+                                   verbose=True)
+
+print("Number of vertices : {nb_vertices}".format(nb_vertices=len(vertices)))
+print("Number of faces : {nb_faces}".format(nb_faces=len(faces)))
